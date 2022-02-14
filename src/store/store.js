f5728ee2695010bd30ab1e87a1d57e2820b1d004
sweetereater <sweeter_2093@mail.ru>
2022-02-14 11:18:53 +0300

    added access and user slices in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import savedTracksSlice from "./features/savedTracks/savedTracksSlice";
 import playerActiveTracksSlice from "./features/playerActiveTracks/playerActiveTracksSlice";
+import accessSlice from './features/access/accessSlice';
+import userSlice from "./features/user/userSlice";
 
 export const store = configureStore({
     reducer: {
+        access: accessSlice,
+        userInfo: userSlice,
         savedTracks: savedTracksSlice,
         playerActiveTracks: playerActiveTracksSlice,
     }
