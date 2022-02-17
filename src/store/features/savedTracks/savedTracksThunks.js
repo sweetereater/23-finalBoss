c07ea251e9ce466209c0d86855b17184fe426d73
sweetereater <sweeter_2093@mail.ru>
2022-02-18 00:34:56 +0300

    added ability to add/remove tracks to/from 'saved'

--- src/store/features/savedTracks/savedTracksThunks.js
+++ src/store/features/savedTracks/savedTracksThunks.js
@@ -1,9 +1,27 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { spotifyApi } from '../../spotifyAPI'
-import { setSavedTracks } from './savedTracksSlice'
-import { setPlayerActiveTracks } from "../playerActiveTracks/playerActiveTracksSlice";
+import { setSavedTracks, removeTrack, addTrack } from './savedTracksSlice'
 
 export const getSavedTracks = createAsyncThunk('savedTracks/getSavedTracks', async (_, { dispatch }) => {
     const response = await spotifyApi.getMySavedTracks();
     dispatch(setSavedTracks(response.body.items.map((item) => item.track)));
+})
+
+export const addTrackToSaved = createAsyncThunk('savedTracks/addTrackToSaved', async (track, { dispatch }) => {
+    try {
+        const response = await spotifyApi.addToMySavedTracks([track.id]);
+        dispatch(addTrack(track));
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/* мб какую красивую анимацию прикрутить тут? */
+export const removeTrackFromSaved = createAsyncThunk('savedTracks/removeTrackFromSaved', async (id, { dispatch }) => {
+    try {
+        const response = await spotifyApi.removeFromMySavedTracks([id]);
+        dispatch(removeTrack(id))
+    } catch (error) {
+        console.log(error)
+    }
 })
