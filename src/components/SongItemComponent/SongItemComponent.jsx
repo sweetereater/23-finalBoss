eb8e978df27473443f87337aaab4ca10164da9be
sweetereater <sweeter_2093@mail.ru>
2022-02-18 00:36:11 +0300

    modified SongItemComponent, added ability to like/dislike track

--- src/components/SongItemComponent/SongItemComponent.jsx
+++ src/components/SongItemComponent/SongItemComponent.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useCallback } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -7,23 +7,43 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentTrack, setIsPlaying, setMusicSource } from '../../store/features/playerActiveTracks/playerActiveTracksSlice';
 import { currentMusicSourceSelector, currentTrackSelector, isPlayingSelector } from '../../store/features/playerActiveTracks/activeTracksSelectors';
-
 import { setPlayerActiveTracks } from '../../store/features/playerActiveTracks/playerActiveTracksSlice';
 import { currentTracksSelector } from '../../store/features/currentTracks/currentTracksSelector';
-import { useCallback } from 'react';
+import { savedTracksSelector } from '../../store/features/savedTracks/savedTracksSelectors';
+import { addTrackToSaved, removeTrackFromSaved } from '../../store/features/savedTracks/savedTracksThunks';
+import { getSongDuration } from '../../utils/timeFunctions';
 
 function SongItemComponent(props) {
 
-    const { name, duration, img, order, source } = props;
+    const { track, order, source } = props;
+    const { id, name } = track;
+    const duration = getSongDuration(track.duration_ms);
+    const img = track.album.images[2].url;
+
     const currentTrack = useSelector(currentTrackSelector);
     const isPlaying = useSelector(isPlayingSelector);
     const musicSrc = useSelector(currentMusicSourceSelector)
-    
+
     const currentTracks = useSelector(currentTracksSelector)
     const dispatch = useDispatch();
+
+
+    const savedTracks = useSelector(savedTracksSelector)
+    const isSaved = savedTracks.find(tracks => tracks.id === id)
+
+    const iconColor = '#0083f5';
+    const iconStyles = {
+        fill: iconColor,
+        height: 38,
+        width: 38,
+        cursor: 'pointer',
+    }
 
     const handleClick = useCallback(() => {
         /* TODO -> написать логику, учесть варианты:
@@ -39,13 +59,21 @@ function SongItemComponent(props) {
 
         if (currentTrack === order && (source === musicSrc || musicSrc === null)) {
             dispatch(setIsPlaying(!isPlaying))
-        } 
-      
+        }
+
         if (currentTrack !== order && source === musicSrc) {
             dispatch(setCurrentTrack(order))
             dispatch(setIsPlaying(true))
         }
     }, [currentTrack, currentTracks, dispatch, isPlaying, musicSrc, order, source])
+
+    const handleTrackLike = useCallback(() => {
+        if (!isSaved) {
+            dispatch(addTrackToSaved(track))
+        } else {
+            dispatch(removeTrackFromSaved(id))
+        }
+    }, [track]);
 
     const TinyText = styled(Typography)({
         fontSize: '0.75rem',
@@ -64,24 +92,33 @@ function SongItemComponent(props) {
             margin: '10px 0',
             bgcolor: (source === musicSrc && currentTrack === order) ? '#a7e0fc' : "#fff"
         }}>
-            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1, }}>
                     <img src={img} alt="" />
                     <IconButton aria-label="play/pause" onClick={handleClick} >
                         {
                             (source === musicSrc && currentTrack === order) && isPlaying ?
-                                <PauseIcon sx={{ height: 38, width: 38, fill: '#0083f5' }} /> :
-                                <PlayArrowIcon sx={{ height: 38, width: 38, fill: '#0083f5' }} />
+                                <PauseIcon sx={iconStyles} /> :
+                                <PlayArrowIcon sx={iconStyles} />
                         }
                     </IconButton>
                 </Box>
-                <CardContent sx={{ display: 'flex', flexDirection: 'row', flex: '1 0 auto' }}>
+                <CardContent sx={{ display: 'flex', flexDirection: 'row', flex: '1 0 auto', alignItems: 'center' }}>
                     <Typography component="div" variant="subtitle1">
                         {name}
                     </Typography>
                 </CardContent>
             </Box>
-            <TinyText>{duration}</TinyText>
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <Box onClick={handleTrackLike}>
+                    {isSaved ?
+                        <FavoriteIcon sx={{ ...iconStyles, height: 24, width: 24 }} /> :
+                        <FavoriteBorderIcon sx={{ ...iconStyles, height: 24, width: 24 }} />
+                    }
+                </Box>
+                <TinyText>{duration}</TinyText>
+            </Box>
+
         </Card>
     );
 }
