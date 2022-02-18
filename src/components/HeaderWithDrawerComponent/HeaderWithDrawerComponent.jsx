ebfe2e855e907a8858ccff29dbfa7fd43b7496c1
sweetereater <sweeter_2093@mail.ru>
2022-02-19 00:45:31 +0300

    removed console.log's

--- src/components/HeaderWithDrawerComponent/HeaderWithDrawerComponent.jsx
+++ src/components/HeaderWithDrawerComponent/HeaderWithDrawerComponent.jsx
@@ -14,71 +14,71 @@ import SearchIcon from '@mui/icons-material/Search';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import { BrowserRouter } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './HeaderWithDrawerComponent.css'
 
 const drawerWidth = 240;
 
 export default function HeaderWithDrawerComponent() {
     return (
-            <Box sx={{ display: 'flex' }}>
-                <CssBaseline />
-                <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                    <Toolbar>
-                        <img className='Header_Icon' src="https://img.icons8.com/dusk/50/000000/spotify.png"/>
-                        <Typography variant="h6" noWrap component="div">
-                            Best player ever
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    className='drawer'
-                    variant="permanent"
-                    sx={{
-                        width: drawerWidth,
-                        flexShrink: 0,
-                        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-                    }}
-                >
-                    <Toolbar />
-                    <Box sx={{overflow: 'auto'}}>
-                        <Link to='/search'>
-                            <List>
-                                <ListItem button key={'Search'}>
-                                    <ListItemIcon>
-                                        <SearchIcon/>
-                                    </ListItemIcon>
-                                    <ListItemText primary={'Search'} onClick={() => console.log('search')}/>
-                                </ListItem>
-                            </List>
-                        </Link>
-                        <Divider/>
-                        <Link to='/music'>
-                            <List>
-                                <ListItem button key={'Favorite songs'}>
-                                    <ListItemIcon>
-                                        <FavoriteBorderIcon/>
-                                    </ListItemIcon>
-                                    <ListItemText primary={'Favorite songs'} onClick={() => console.log('favorite')}/>
-                                </ListItem>
-                            </List>
-                        </Link>
-                        <Divider />
-                        <Link to='/playlists'>
-                            <List>
-                                <ListItem button key={'Playlists'}>
-                                    <ListItemIcon>
-                                        <ListAltIcon/>
-                                    </ListItemIcon>
-                                    <ListItemText primary={'Playlists'} onClick={() => console.log('playlists')}/>
-                                </ListItem>
-                            </List>
-                        </Link>
-                    </Box>
-                </Drawer>
-                <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
-                    <Toolbar />
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar>
+                    <img className='Header_Icon' src="https://img.icons8.com/dusk/50/000000/spotify.png" />
+                    <Typography variant="h6" noWrap component="div">
+                        Best player ever
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className='drawer'
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+            >
+                <Toolbar />
+                <Box sx={{ overflow: 'auto' }}>
+                    <NavLink to='/search'>
+                        <List>
+                            <ListItem button key={'Search'}>
+                                <ListItemIcon>
+                                    <SearchIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Search'} />
+                            </ListItem>
+                        </List>
+                    </NavLink>
+                    <Divider />
+                    <NavLink to='/music'>
+                        <List>
+                            <ListItem button key={'Favorite songs'}>
+                                <ListItemIcon>
+                                    <FavoriteBorderIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Favorite songs'} />
+                            </ListItem>
+                        </List>
+                    </NavLink>
+                    <Divider />
+                    <NavLink to='/playlists'>
+                        <List>
+                            <ListItem button key={'Playlists'}>
+                                <ListItemIcon>
+                                    <ListAltIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Playlists'} />
+                            </ListItem>
+                        </List>
+                    </NavLink>
                 </Box>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
+                <Toolbar />
             </Box>
+        </Box>
     );
 }
