958cf873e9266224a2439dec7731afe5d83652bf
sweetereater <sweeter_2093@mail.ru>
2022-02-19 00:47:30 +0300

    added action for adding tracks to playlist

--- src/store/features/playlists/playlistsSlice.js
+++ src/store/features/playlists/playlistsSlice.js
@@ -1,3 +1,4 @@
+import { Plumbing } from "@mui/icons-material";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -10,10 +11,14 @@ export const playlistsSlice = createSlice({
     reducers: {
         setPlaylists: (state, action) => {
             state.playlists = action.payload
+        },
+        addTracksToPlaylist: (state, action) => {
+            console.log(action);
+            state.playlists = state.playlists.map(pl => pl.id === action.payload.id ? { ...pl, tracks: action.payload.tracks } : pl)
         }
     },
 })
 
-export const { setPlaylists } = playlistsSlice.actions;
+export const { setPlaylists, addTracksToPlaylist } = playlistsSlice.actions;
 
 export default playlistsSlice.reducer;
