307f57076c5496573823622e7c5ed6aaad1c0f3a
sweetereater <sweeter_2093@mail.ru>
2022-02-20 00:17:58 +0300

    added search in savedTracks

--- src/components/MySavedTracks/MySavedTracks.jsx
+++ src/components/MySavedTracks/MySavedTracks.jsx
@@ -1,12 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { savedTracksSelector } from "../../store/features/savedTracks/savedTracksSelectors";
 import { getSavedTracks } from '../../store/features/savedTracks/savedTracksThunks';
 import { tokenSelector } from '../../store/features/access/accessSelectors';
 import Tracks from '../Tracks/Tracks';
-import { Box, CircularProgress } from '@mui/material';
+import { Box, Input, CircularProgress } from '@mui/material';
 import { loaderSelector } from '../../store/features/loader/loaderSelectors';
+import useDebounce from '../SearchPage/debounce';
 
 
 const MySavedTracks = () => {
@@ -19,15 +20,33 @@ const MySavedTracks = () => {
     }
   }, [token])
 
+  const [searchValue, setSearchValue] = useState('');
+  const debouncedSearch = useDebounce(searchValue, 1000)
+
+  const handleInput = useCallback((e) => {
+    setSearchValue(e.target.value)
+  }, [searchValue])
+
+
   const isLoading = useSelector(loaderSelector);
 
   if (isLoading) return <CircularProgress />
 
   if (!token) return <Redirect to='/login' />
+  console.log(tracks);
+
+  const filteredTracks = tracks.filter(track => {
+    const searchingFor = debouncedSearch.toLowerCase()
+    const trackName = track.name.toLowerCase()
+    const artistsString = track.artists.map(artist => artist.name).join(', ').toLowerCase();
+
+    return trackName.includes(searchingFor) || artistsString.includes(searchingFor)
+  })
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '280px' }}>
-      <Tracks tracks={tracks} source="/music" width={1700} />
+      <Input style={{ width: '90%', marginBottom: '2rem', }} placeholder="Let's find something interesting" value={searchValue} onChange={handleInput}></Input>
+      <Tracks tracks={filteredTracks} source="/music" width={1600} />
     </Box>)
 }
 
