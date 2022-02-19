12775ae7115e24296e542c5b565bec78330b716c
sweetereater <sweeter_2093@mail.ru>
2022-02-19 20:31:39 +0300

    download all saved tracks

--- src/store/features/savedTracks/savedTracksThunks.js
+++ src/store/features/savedTracks/savedTracksThunks.js
@@ -1,10 +1,39 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { spotifyApi } from '../../spotifyAPI'
 import { setSavedTracks, removeTrack, addTrack } from './savedTracksSlice'
+import { setLoading } from '../loader/loaderSlice';
 
 export const getSavedTracks = createAsyncThunk('savedTracks/getSavedTracks', async (_, { dispatch }) => {
-    const response = await spotifyApi.getMySavedTracks();
-    dispatch(setSavedTracks(response.body.items.map((item) => item.track)));
+    let savedTracks = [];
+    let offset = 0;
+    let limit = 50;
+
+    try {
+        dispatch(setLoading(true))
+        const response = await spotifyApi.getMySavedTracks({ limit });
+        const amountOfTracks = response.body.total;
+        console.log('total tracks: ', amountOfTracks)
+
+        savedTracks = [...savedTracks, ...response.body.items.map((item) => item.track)]
+
+        if (response.body.items.length < amountOfTracks) {
+            let requests = [];
+            while (offset + limit < amountOfTracks) {
+                requests.push(spotifyApi.getMySavedTracks({ offset: offset += limit, limit }))
+            }
+            const responses = await Promise.all(requests);
+            console.log('Responses: ', responses)
+            responses.forEach(response => savedTracks = [...savedTracks, ...response.body.items.map((item) => item.track)])
+        }
+
+        console.log('got from server: ', savedTracks.length)
+        dispatch(setLoading(false))
+        dispatch(setSavedTracks(savedTracks));
+
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 export const addTrackToSaved = createAsyncThunk('savedTracks/addTrackToSaved', async (track, { dispatch }) => {
