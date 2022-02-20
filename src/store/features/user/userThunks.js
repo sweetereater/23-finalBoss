be64c51a608c7bd8d1595a9ed0c574ed8f3222a9
sweetereater <sweeter_2093@mail.ru>
2022-02-20 14:04:33 +0300

    some logic in userSlice

--- src/store/features/user/userThunks.js
+++ src/store/features/user/userThunks.js
@@ -1,11 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { spotifyApi } from '../../spotifyAPI';
 import { setAccessToken } from "../access/accessSlice";
+import { setUser } from "./userSlice";
 
 export const getUserInfo = createAsyncThunk('savedTracks/getSavedTracks', async (accessToken, { dispatch }) => {
     try {
         const response = await spotifyApi.getMe();
         if (response.statusCode === 200) {
+            const { display_name, id } = response.body;
+            dispatch(setUser({
+                display_name, id
+            }))
             dispatch(setAccessToken(accessToken))
         }
     } catch (error) {
