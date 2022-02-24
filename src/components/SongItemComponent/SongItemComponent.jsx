7323a110b364ec8904c8bdd294e57795e086f383
sweetereater <sweeter_2093@mail.ru>
2022-02-24 15:03:58 +0300

    added import useState

--- src/components/SongItemComponent/SongItemComponent.jsx
+++ src/components/SongItemComponent/SongItemComponent.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
